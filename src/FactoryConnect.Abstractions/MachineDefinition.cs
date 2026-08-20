02dfc014e816a73fa2278a440df12eb5d8b4d38c
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-08-20 16:01:45 +0530

    FC-002: use production line identity

--- src/FactoryConnect.Abstractions/MachineDefinition.cs
+++ src/FactoryConnect.Abstractions/MachineDefinition.cs
@@ -4,6 +4,6 @@ public sealed record MachineDefinition
 {
     public required MachineId Id { get; init; }
     public required string Name { get; init; }
-    public required string LineId { get; init; }
+    public required ProductionLineId LineId { get; init; }
     public IReadOnlyList<MachineSignalDefinition> Signals { get; init; } = [];
 }
