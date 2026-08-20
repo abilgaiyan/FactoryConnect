7971cb6ce5907b1f925b2808f8b8058e2f572f87
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-08-20 16:02:31 +0530

    FC-002: exercise simulator connector

--- samples/FactoryConnect.Simulator/Program.cs
+++ samples/FactoryConnect.Simulator/Program.cs
@@ -1,4 +1,14 @@
 using FactoryConnect.Abstractions;
+using FactoryConnect.Simulator;
 
 var machine = MachineId.New();
+var connector = new SimulatedMachineConnector(machine);
+
 Console.WriteLine($"FactoryConnect Simulator initialized for machine {machine}.");
+
+for (var i = 0; i < 8; i++)
+{
+    var snapshot = await connector.ReadSignalsAsync();
+    var values = string.Join(", ", snapshot.Signals.Select(signal => $"{signal.Key}={signal.Value}"));
+    Console.WriteLine($"{snapshot.Timestamp:O} | {values}");
+}
