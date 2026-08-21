8305b2ca06b955c4fba025222f2f2e8136156d8e
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-08-21 20:14:16 +0530

    FC-010 add ELRE calculation strategy key

--- src/FactoryConnect.Abstractions/MetricStrategyKeys.cs
+++ src/FactoryConnect.Abstractions/MetricStrategyKeys.cs
@@ -3,6 +3,7 @@ namespace FactoryConnect.Abstractions;
 public static class MetricStrategyKeys
 {
     public const string AptOverPot = "apt-over-pot";
+    public const string AptOverMachinePowerOnTime = "apt-over-machine-power-on-time";
     public const string ReferenceTimeOverApt = "reference-time-over-apt";
     public const string GoodOverProduced = "good-over-produced";
     public const string AvailabilityPerformanceQuality =
