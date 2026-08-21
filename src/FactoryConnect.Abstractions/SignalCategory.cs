6ce3f86b036b9d6bc96da7c88290754a6840c81d
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-08-21 11:25:16 +0530

    feat: add canonical signal categories

---
+++ src/FactoryConnect.Abstractions/SignalCategory.cs
@@ -0,0 +1,11 @@
+namespace FactoryConnect.Abstractions;
+
+public enum SignalCategory
+{
+    State = 0,
+    Production = 1,
+    Process = 2,
+    Condition = 3,
+    Energy = 4,
+    Custom = 5,
+}
