4a140fb67d59d7dd199552621eca994d31baedb4
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-08-27 01:00:22 +0530

    Add offline duration metric input fact

--- src/FactoryConnect.Abstractions/MetricInputFactKeys.cs
+++ src/FactoryConnect.Abstractions/MetricInputFactKeys.cs
@@ -8,6 +8,7 @@ public static class MetricInputFactKeys
     public const string IdleDuration = "duration.idle";
     public const string StoppedDuration = "duration.stopped";
     public const string AlarmDuration = "duration.alarm";
+    public const string OfflineDuration = "duration.offline";
     public const string PartCountIncrement = "quantity.part-count-increment";
     public const string GoodQuantity = "quantity.good";
     public const string RejectedQuantity = "quantity.rejected";
