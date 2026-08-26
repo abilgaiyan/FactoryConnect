d64d7ad3a09601eb26d317453db1052d127da862
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-08-26 23:17:09 +0530

    Add contextualized activity interval contract

---
+++ src/FactoryConnect.Abstractions/ContextualizedActivityInterval.cs
@@ -0,0 +1,24 @@
+namespace FactoryConnect.Abstractions;
+
+public sealed record ContextualizedActivityInterval
+{
+    public required ContextualizedActivityIntervalId Id { get; init; }
+    public required ObservationProcessorId SourceProcessorId { get; init; }
+    public required ObservationPosition SourcePosition { get; init; }
+    public required ObservationStreamId SourceStreamId { get; init; }
+    public required ulong SourceInstanceId { get; init; }
+    public required ulong SourceSequence { get; init; }
+    public required MachineId MachineId { get; init; }
+    public required MachineState State { get; init; }
+    public required DateTimeOffset StartsAtUtc { get; init; }
+    public required DateTimeOffset EndsAtUtc { get; init; }
+    public required ShiftId ShiftId { get; init; }
+    public required ShiftScheduleAssignmentId ShiftScheduleAssignmentId { get; init; }
+    public ProductionContextAssignmentId? ProductionContextAssignmentId { get; init; }
+    public ProductionOrderId? ProductionOrderId { get; init; }
+    public OperationId? OperationId { get; init; }
+    public PartId? PartId { get; init; }
+    public OperatorId? OperatorId { get; init; }
+
+    public TimeSpan Duration => EndsAtUtc - StartsAtUtc;
+}
