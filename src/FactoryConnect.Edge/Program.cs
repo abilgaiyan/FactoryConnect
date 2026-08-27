613ec9ab925cbb6e93466a0d4e3ece55e16d5aa0
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-08-27 23:54:39 +0530

    FC-026.6 use shared Edge application composition root

--- src/FactoryConnect.Edge/Program.cs
+++ src/FactoryConnect.Edge/Program.cs
@@ -1,24 +1,5 @@
 using FactoryConnect.Edge;
 
 var builder = Host.CreateApplicationBuilder(args);
-var machineInventory = MtConnectMachineInventory.FromConfiguration(
-    builder.Configuration);
-var activityStreams = machineInventory.ActivityStreams;
-var machineIds = machineInventory.MachineIds;
-
-builder.Services.AddFactoryConnectEdgePersistence(
-    builder.Configuration);
-builder.Services.AddFactoryConnectObservationProcessing(
-    builder.Configuration,
-    activityStreams);
-builder.Services.AddFactoryConnectProductionMetricInputs(
-    builder.Configuration,
-    activityStreams);
-builder.Services.AddFactoryConnectMetricAggregation(
-    builder.Configuration,
-    machineIds);
-builder.Services.AddFactoryConnectMtConnectAcquisition(
-    builder.Configuration,
-    machineInventory);
-
+builder.Services.AddFactoryConnectEdgeApplication(builder.Configuration);
 await builder.Build().RunAsync();
