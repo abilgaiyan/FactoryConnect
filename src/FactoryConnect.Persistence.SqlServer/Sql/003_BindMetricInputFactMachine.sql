66ce051077059e04e1e818b642f9e1b275358e89
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-08-27 18:08:28 +0530

    Make FC-026.5 machine binding migration atomic

--- src/FactoryConnect.Persistence.SqlServer/Sql/003_BindMetricInputFactMachine.sql
+++ src/FactoryConnect.Persistence.SqlServer/Sql/003_BindMetricInputFactMachine.sql
@@ -1,20 +1,37 @@
-ALTER TABLE dbo.MetricInputFact
-    DROP CONSTRAINT FK_MetricInputFact_MetricInputStream;
+SET XACT_ABORT ON;
 
-ALTER TABLE dbo.MetricInputStream
-    ADD CONSTRAINT UQ_MetricInputStream_RowMachine
-        UNIQUE (
-            MetricInputStreamRowId,
-            MachineId
-        );
+BEGIN TRY
+    BEGIN TRANSACTION;
 
-ALTER TABLE dbo.MetricInputFact
-    ADD CONSTRAINT FK_MetricInputFact_StreamMachine
-        FOREIGN KEY (
-            MetricInputStreamRowId,
-            MachineId
-        )
-        REFERENCES dbo.MetricInputStream (
-            MetricInputStreamRowId,
-            MachineId
-        );
+    ALTER TABLE dbo.MetricInputFact
+        DROP CONSTRAINT FK_MetricInputFact_MetricInputStream;
+
+    ALTER TABLE dbo.MetricInputStream
+        ADD CONSTRAINT UQ_MetricInputStream_RowMachine
+            UNIQUE (
+                MetricInputStreamRowId,
+                MachineId
+            );
+
+    ALTER TABLE dbo.MetricInputFact WITH CHECK
+        ADD CONSTRAINT FK_MetricInputFact_StreamMachine
+            FOREIGN KEY (
+                MetricInputStreamRowId,
+                MachineId
+            )
+            REFERENCES dbo.MetricInputStream (
+                MetricInputStreamRowId,
+                MachineId
+            );
+
+    ALTER TABLE dbo.MetricInputFact
+        CHECK CONSTRAINT FK_MetricInputFact_StreamMachine;
+
+    COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRANSACTION;
+
+    THROW;
+END CATCH;
