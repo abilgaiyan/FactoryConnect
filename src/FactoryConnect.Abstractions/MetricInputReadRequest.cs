c2122bb7884dbb6e8dfd93c0a8706131bd9d7874
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-08-27 14:27:22 +0530

    FC-026.1 bind read requests to checkpoint stream

--- src/FactoryConnect.Abstractions/MetricInputReadRequest.cs
+++ src/FactoryConnect.Abstractions/MetricInputReadRequest.cs
@@ -21,9 +21,33 @@ public sealed record MetricInputReadRequest
         MaxCount = maxCount;
     }
 
+    public MetricInputReadRequest(
+        MetricInputStreamId streamId,
+        MetricAggregationCheckpoint? checkpoint,
+        int maxCount)
+        : this(streamId, GetCheckpointPosition(streamId, checkpoint), maxCount)
+    {
+    }
+
     public MetricInputStreamId StreamId { get; }
 
     public MetricInputPosition? AfterPosition { get; }
 
     public int MaxCount { get; }
+
+    private static MetricInputPosition? GetCheckpointPosition(
+        MetricInputStreamId streamId,
+        MetricAggregationCheckpoint? checkpoint)
+    {
+        ArgumentNullException.ThrowIfNull(streamId);
+
+        if (checkpoint is not null && checkpoint.StreamId != streamId)
+        {
+            throw new ArgumentException(
+                "Aggregation checkpoint must belong to the requested metric input stream.",
+                nameof(checkpoint));
+        }
+
+        return checkpoint?.Position;
+    }
 }
