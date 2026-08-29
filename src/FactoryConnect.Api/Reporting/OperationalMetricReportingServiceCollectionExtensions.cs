58bf63df06df532a3fe363e8e751ea991089812e
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-08-29 18:40:13 +0530

    FC-028.6 add reporting API composition

---
+++ src/FactoryConnect.Api/Reporting/OperationalMetricReportingServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+using FactoryConnect.Abstractions;
+using FactoryConnect.Core.Metrics;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace FactoryConnect.Api.Reporting;
+
+public static class OperationalMetricReportingServiceCollectionExtensions
+{
+    public static IServiceCollection AddFactoryConnectOperationalMetricReporting(
+        this IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services.TryAddSingleton<IOperationalMetricReportingQueryReader>(
+            static provider => new OperationalMetricReportingQueryReader(
+                provider.GetRequiredService<IOperationalMetricReportingQueryProvider>()));
+        services.TryAddSingleton<IOperationalMetricQueryReader>(
+            static provider => new OperationalMetricQueryReader(
+                provider.GetRequiredService<IOperationalMetricReportingQueryReader>()));
+
+        return services;
+    }
+}
