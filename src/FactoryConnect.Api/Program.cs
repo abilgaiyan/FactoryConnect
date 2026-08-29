89b81d7343edb2fa44fa878f22d702d641b3f362
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-08-29 18:41:58 +0530

    FC-028.6 select reporting persistence provider

--- src/FactoryConnect.Api/Program.cs
+++ src/FactoryConnect.Api/Program.cs
@@ -1,13 +1,14 @@
-using FactoryConnect.Abstractions;
 using FactoryConnect.Api.Reporting;
-using FactoryConnect.Core;
+using FactoryConnect.Infrastructure;
+using FactoryConnect.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
-builder.Services.AddSingleton<InMemoryOperationalMetricProjectionStore>();
-builder.Services.AddSingleton<IOperationalMetricReportingQueryProvider>(
-    static provider => provider.GetRequiredService<InMemoryOperationalMetricProjectionStore>());
+builder.Services.AddInMemoryPersistenceProvider();
+builder.Services.AddFactoryConnectPersistence(
+    builder.Configuration,
+    PersistenceProviderCapabilities.Reporting);
 builder.Services.AddFactoryConnectOperationalMetricReporting();
 
 var app = builder.Build();
