e0b7f94dfcafffac7ff776285330ad65c1c4c771
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-09-03 22:03:34 +0530

    feat: add explicit SQL owned-object recognition set

---
+++ src/FactoryConnect.Persistence.SqlServer/SqlOwnedObjectRecognitionSet.cs
@@ -0,0 +1,21 @@
+using System.Collections.Immutable;
+
+namespace FactoryConnect.Persistence.SqlServer;
+
+internal sealed class SqlOwnedObjectRecognitionSet
+{
+    public SqlOwnedObjectRecognitionSet(IEnumerable<SqlObjectName> ownedTables)
+    {
+        ArgumentNullException.ThrowIfNull(ownedTables);
+
+        OwnedTables = ownedTables
+            .Distinct()
+            .OrderBy(static table => table.SchemaName, StringComparer.Ordinal)
+            .ThenBy(static table => table.ObjectName, StringComparer.Ordinal)
+            .ToImmutableArray();
+    }
+
+    public ImmutableArray<SqlObjectName> OwnedTables { get; }
+
+    public bool Recognizes(SqlObjectName table) => OwnedTables.Contains(table);
+}
