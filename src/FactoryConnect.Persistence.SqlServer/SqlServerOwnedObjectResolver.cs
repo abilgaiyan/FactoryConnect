142b40de480eebad67937f0cc98d88a3cdd36ae5
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-09-03 22:33:56 +0530

    feat: add SQL-resolved owned object identity reader

---
+++ src/FactoryConnect.Persistence.SqlServer/SqlServerOwnedObjectResolver.cs
@@ -0,0 +1,59 @@
+using System.Collections.Immutable;
+using Microsoft.Data.SqlClient;
+
+namespace FactoryConnect.Persistence.SqlServer;
+
+internal sealed record SqlResolvedObject(
+    SqlObjectName RepositoryIdentity,
+    SqlObjectName CatalogIdentity,
+    int ObjectId);
+
+internal sealed class SqlServerOwnedObjectResolver
+{
+    public async Task<ImmutableArray<SqlResolvedObject>> ResolveAsync(
+        SqlConnection connection,
+        SqlOwnedObjectRecognitionSet recognitionSet,
+        CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(connection);
+        ArgumentNullException.ThrowIfNull(recognitionSet);
+
+        var resolved = ImmutableArray.CreateBuilder<SqlResolvedObject>();
+        foreach (var repositoryIdentity in recognitionSet.OwnedTables)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            await using var command = connection.CreateCommand();
+            command.CommandText = """
+                SELECT
+                    t.object_id,
+                    s.name AS SchemaName,
+                    t.name AS TableName
+                FROM sys.tables AS t
+                INNER JOIN sys.schemas AS s
+                    ON s.schema_id = t.schema_id
+                WHERE t.object_id = OBJECT_ID(
+                    QUOTENAME(@SchemaName) + N'.' + QUOTENAME(@TableName),
+                    N'U');
+                """;
+            command.Parameters.AddWithValue("@SchemaName", repositoryIdentity.SchemaName);
+            command.Parameters.AddWithValue("@TableName", repositoryIdentity.ObjectName);
+
+            await using var reader = await command.ExecuteReaderAsync(cancellationToken);
+            if (!await reader.ReadAsync(cancellationToken))
+            {
+                continue;
+            }
+
+            resolved.Add(new SqlResolvedObject(
+                repositoryIdentity,
+                new SqlObjectName(reader.GetString(1), reader.GetString(2)),
+                reader.GetInt32(0)));
+        }
+
+        return resolved
+            .OrderBy(static item => item.RepositoryIdentity.SchemaName, StringComparer.Ordinal)
+            .ThenBy(static item => item.RepositoryIdentity.ObjectName, StringComparer.Ordinal)
+            .ToImmutableArray();
+    }
+}
