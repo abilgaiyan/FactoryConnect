f14c5bf8dd1439ccfef64974c61135d41d8886a5
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-09-05 12:31:12 +0530

    feat: gate Edge startup on persistence readiness

--- src/FactoryConnect.Edge/Program.cs
+++ src/FactoryConnect.Edge/Program.cs
@@ -2,4 +2,12 @@ using FactoryConnect.Edge;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddFactoryConnectEdgeApplication(builder.Configuration);
-await builder.Build().RunAsync();
+builder.Services.AddSingleton<IEdgeStartupCancellationRegistrationFactory,
+    EdgeStartupCancellationRegistrationFactory>();
+
+var host = builder.Build();
+
+await EdgePersistenceStartup.RunAsync(
+    host.Services,
+    () => host.RunAsync(),
+    host.Services.GetRequiredService<IEdgeStartupCancellationRegistrationFactory>());
