f7b80a376d6de925c991546a9458742a2aa101f6
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-09-08 16:18:07 +0530

    fix: enforce D.2 projection checkpoint revision coherence

--- src/FactoryConnect.Persistence.SqlServer/SqlServerOperationalMetricProjectionStableRead.cs
+++ src/FactoryConnect.Persistence.SqlServer/SqlServerOperationalMetricProjectionStableRead.cs
@@ -1,4 +1,5 @@
 using System.Data;
+using FactoryConnect.Abstractions;
 using Microsoft.Data.SqlClient;
 
 namespace FactoryConnect.Persistence.SqlServer;
@@ -7,10 +8,25 @@ internal static class SqlServerOperationalMetricProjectionStableRead
 {
     internal const int MaximumAttempts = 3;
 
-    public static async Task<T> ExecuteAsync<T>(
+    public static Task<T> ExecuteAsync<T>(
         SqlConnection connection,
         long projectionProcessorRowId,
         Func<SqlConnection, CancellationToken, Task<T>> readPublication,
+        CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(readPublication);
+
+        return ExecuteAsync(
+            connection,
+            projectionProcessorRowId,
+            (stableConnection, _, token) => readPublication(stableConnection, token),
+            cancellationToken);
+    }
+
+    public static async Task<T> ExecuteAsync<T>(
+        SqlConnection connection,
+        long projectionProcessorRowId,
+        Func<SqlConnection, MetricInputPosition?, CancellationToken, Task<T>> readPublication,
         CancellationToken cancellationToken)
     {
         ArgumentNullException.ThrowIfNull(connection);
@@ -32,7 +48,13 @@ internal static class SqlServerOperationalMetricProjectionStableRead
                 projectionProcessorRowId,
                 cancellationToken);
 
-            var result = await readPublication(connection, cancellationToken);
+            await ValidateManifestRevisionCoherenceAsync(
+                connection,
+                projectionProcessorRowId,
+                checkpointBefore,
+                cancellationToken);
+
+            var result = await readPublication(connection, checkpointBefore, cancellationToken);
 
             cancellationToken.ThrowIfCancellationRequested();
 
@@ -51,7 +73,7 @@ internal static class SqlServerOperationalMetricProjectionStableRead
             "Operational metric projection publication did not remain checkpoint-stable within the configured read retry budget.");
     }
 
-    private static async Task<decimal?> ReadCheckpointPositionAsync(
+    private static async Task<MetricInputPosition?> ReadCheckpointPositionAsync(
         SqlConnection connection,
         long projectionProcessorRowId,
         CancellationToken cancellationToken)
@@ -69,6 +91,50 @@ internal static class SqlServerOperationalMetricProjectionStableRead
             return null;
         }
 
-        return Convert.ToDecimal(value, System.Globalization.CultureInfo.InvariantCulture);
+        var materialized = SqlServerUInt64.Materialize(
+            Convert.ToDecimal(value, System.Globalization.CultureInfo.InvariantCulture));
+        return new MetricInputPosition(materialized);
+    }
+
+    private static async Task ValidateManifestRevisionCoherenceAsync(
+        SqlConnection connection,
+        long projectionProcessorRowId,
+        MetricInputPosition? checkpointPosition,
+        CancellationToken cancellationToken)
+    {
+        await using var command = connection.CreateCommand();
+        command.Parameters.Add("@ProcessorRowId", SqlDbType.BigInt).Value = projectionProcessorRowId;
+
+        if (checkpointPosition is null)
+        {
+            command.CommandText =
+                "SELECT COUNT_BIG(*) " +
+                "FROM dbo.OperationalMetricProjectionManifest " +
+                "WHERE OperationalMetricProjectionProcessorRowId = @ProcessorRowId;";
+        }
+        else
+        {
+            command.CommandText =
+                "SELECT COUNT_BIG(*) " +
+                "FROM dbo.OperationalMetricProjectionManifest AS m " +
+                "INNER JOIN dbo.OperationalMetricProjection AS p " +
+                "ON p.OperationalMetricProjectionProcessorRowId = m.OperationalMetricProjectionProcessorRowId " +
+                "AND p.OperationalMetricProjectionRowId = m.OperationalMetricProjectionRowId " +
+                "WHERE m.OperationalMetricProjectionProcessorRowId = @ProcessorRowId " +
+                "AND p.SourceRevisionPosition <> @CheckpointPosition;";
+            command.Parameters.Add(
+                SqlServerUInt64.CreateParameter(
+                    "@CheckpointPosition",
+                    checkpointPosition.Value.Value));
+        }
+
+        var mismatchCount = Convert.ToInt64(
+            await command.ExecuteScalarAsync(cancellationToken),
+            System.Globalization.CultureInfo.InvariantCulture);
+        if (mismatchCount != 0)
+        {
+            throw new InvalidOperationException(
+                "Persisted operational metric projection source revision is corrupt or inconsistent with the current publication checkpoint.");
+        }
     }
 }
