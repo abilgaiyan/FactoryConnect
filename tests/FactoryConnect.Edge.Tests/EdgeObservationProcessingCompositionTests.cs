cf7a95089c3de22cc511f4d28c050bfa7d2ed86d
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-09-22 16:34:18 +0530

    tests: align SQL observation capability expectation

--- tests/FactoryConnect.Edge.Tests/EdgeObservationProcessingCompositionTests.cs
+++ tests/FactoryConnect.Edge.Tests/EdgeObservationProcessingCompositionTests.cs
@@ -3,6 +3,7 @@ using FactoryConnect.Core;
 using FactoryConnect.Core.Machines;
 using FactoryConnect.Edge;
 using FactoryConnect.Infrastructure;
+using FactoryConnect.Persistence.SqlServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
@@ -187,7 +188,7 @@ public sealed class EdgeObservationProcessingCompositionTests
     }
 
     [Fact]
-    public void ProviderWithoutProcessingCapabilitiesFailsClearly()
+    public void SqlServerProviderComposesObservationProcessingCapabilities()
     {
         var machineId = MachineId.New();
         var streamId = new ObservationStreamId(machineId, "modbus:line-1");
