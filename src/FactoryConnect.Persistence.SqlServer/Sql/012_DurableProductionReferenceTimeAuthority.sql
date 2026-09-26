9761ea6071080de4ffa0d7257585b8b6748226e0
Ajay Bilgaiyan <a_bilgaiyan@hotmail.com>
2026-09-26 18:49:14 +0530

    fix: align reference-time SQL authority with D17

--- src/FactoryConnect.Persistence.SqlServer/Sql/012_DurableProductionReferenceTimeAuthority.sql
+++ src/FactoryConnect.Persistence.SqlServer/Sql/012_DurableProductionReferenceTimeAuthority.sql
@@ -5,9 +5,9 @@ CREATE TABLE dbo.ProductionReferenceTimeRevision
     CONSTRAINT PK_ProductionReferenceTimeRevision
         PRIMARY KEY (ProductionReferenceTimeRevision),
 
-    CONSTRAINT CK_ProductionReferenceTimeRevision_UInt64Positive
+    CONSTRAINT CK_ProductionReferenceTimeRevision_UInt64
         CHECK (
-            ProductionReferenceTimeRevision >= 1
+            ProductionReferenceTimeRevision >= 0
             AND ProductionReferenceTimeRevision <= 18446744073709551615
         )
 );
@@ -19,18 +19,21 @@ CREATE TABLE dbo.ProductionReferenceTimeOutcome
     CompanyId nvarchar(256) COLLATE Latin1_General_100_BIN2 NOT NULL,
     SiteId nvarchar(256) COLLATE Latin1_General_100_BIN2 NOT NULL,
     MachineId uniqueidentifier NOT NULL,
+    ShiftOccurrenceSiteId nvarchar(256) COLLATE Latin1_General_100_BIN2 NOT NULL,
+    ShiftScheduleAssignmentId nvarchar(256) COLLATE Latin1_General_100_BIN2 NOT NULL,
     ShiftId nvarchar(256) COLLATE Latin1_General_100_BIN2 NOT NULL,
-    ProductionContextAssignmentId nvarchar(256) COLLATE Latin1_General_100_BIN2 NULL,
-    ProductionOrderId nvarchar(256) COLLATE Latin1_General_100_BIN2 NULL,
+    ShiftStartsAtUtc datetimeoffset(7) NOT NULL,
+    ShiftEndsAtUtc datetimeoffset(7) NOT NULL,
+    ProductionDaySiteId nvarchar(256) COLLATE Latin1_General_100_BIN2 NOT NULL,
+    ProductionBusinessDate date NOT NULL,
     OperationId nvarchar(256) COLLATE Latin1_General_100_BIN2 NULL,
     PartId nvarchar(256) COLLATE Latin1_General_100_BIN2 NULL,
-    OperatorId nvarchar(256) COLLATE Latin1_General_100_BIN2 NULL,
     OccurredAtUtc datetimeoffset(7) NOT NULL,
     ProducedQuantity int NOT NULL,
     ProductionStandardAuthorityRevision decimal(20,0) NOT NULL,
     ResolutionStatus tinyint NOT NULL,
     SelectedStandardVersionId nvarchar(256) COLLATE Latin1_General_100_BIN2 NULL,
-    SelectedStandardEffectiveFromUtc datetimeoffset(7) NULL,
+    SelectedStandardSourceReference nvarchar(1024) COLLATE Latin1_General_100_BIN2 NULL,
     IdealProductionDurationSeconds decimal(20,6) NULL,
 
     CONSTRAINT PK_ProductionReferenceTimeOutcome
@@ -40,9 +43,9 @@ CREATE TABLE dbo.ProductionReferenceTimeOutcome
         FOREIGN KEY (ProductionReferenceTimeRevision)
         REFERENCES dbo.ProductionReferenceTimeRevision (ProductionReferenceTimeRevision),
 
-    CONSTRAINT CK_ProductionReferenceTimeOutcome_Revision_UInt64Positive
+    CONSTRAINT CK_ProductionReferenceTimeOutcome_Revision_UInt64
         CHECK (
-            ProductionReferenceTimeRevision >= 1
+            ProductionReferenceTimeRevision >= 0
             AND ProductionReferenceTimeRevision <= 18446744073709551615
         ),
 
@@ -58,13 +61,20 @@ CREATE TABLE dbo.ProductionReferenceTimeOutcome
     CONSTRAINT CK_ProductionReferenceTimeOutcome_Status
         CHECK (ResolutionStatus >= 0 AND ResolutionStatus <= 3),
 
+    CONSTRAINT CK_ProductionReferenceTimeOutcome_PeriodOwnership
+        CHECK (
+            SiteId = ShiftOccurrenceSiteId
+            AND SiteId = ProductionDaySiteId
+            AND ShiftEndsAtUtc > ShiftStartsAtUtc
+            AND OccurredAtUtc >= ShiftStartsAtUtc
+            AND OccurredAtUtc < ShiftEndsAtUtc
+        ),
+
     CONSTRAINT CK_ProductionReferenceTimeOutcome_Utc
         CHECK (
             datepart(tzoffset, OccurredAtUtc) = 0
-            AND (
-                SelectedStandardEffectiveFromUtc IS NULL
-                OR datepart(tzoffset, SelectedStandardEffectiveFromUtc) = 0
-            )
+            AND datepart(tzoffset, ShiftStartsAtUtc) = 0
+            AND datepart(tzoffset, ShiftEndsAtUtc) = 0
         ),
 
     CONSTRAINT CK_ProductionReferenceTimeOutcome_IdealDuration
@@ -100,7 +110,11 @@ CREATE TABLE dbo.ProductionReferenceTimePublicationCut
     ProductionReferenceTimeRevision decimal(20,0) NOT NULL,
 
     CONSTRAINT PK_ProductionReferenceTimePublicationCut
-        PRIMARY KEY (MetricAggregationProcessorRowId, MetricAggregationPosition),
+        PRIMARY KEY (
+            MetricAggregationProcessorRowId,
+            MetricAggregationPosition,
+            ProductionReferenceTimeRevision
+        ),
 
     CONSTRAINT FK_ProductionReferenceTimePublicationCut_MetricAggregationRevision
         FOREIGN KEY (MetricAggregationProcessorRowId, MetricAggregationPosition)
@@ -117,9 +131,9 @@ CREATE TABLE dbo.ProductionReferenceTimePublicationCut
             AND MetricAggregationPosition <= 18446744073709551615
         ),
 
-    CONSTRAINT CK_ProductionReferenceTimePublicationCut_ReferenceRevision_UInt64Positive
+    CONSTRAINT CK_ProductionReferenceTimePublicationCut_ReferenceRevision_UInt64
         CHECK (
-            ProductionReferenceTimeRevision >= 1
+            ProductionReferenceTimeRevision >= 0
             AND ProductionReferenceTimeRevision <= 18446744073709551615
         )
 );
